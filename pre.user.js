ef0ebe8a27a1c77c47c53ababf28a8738c1945c5
phantruongazapi <trungtruongphan116@gmail.com>
2025-02-22 21:51:12 +0700

    Update pre.user.js

--- pre.user.js
+++ pre.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bypass Yeumoney V3
 // @namespace    http://tampermonkey.net/
-// @version      3.1
+// @version      3.2
 // @description  Bypass Yeumoney premium
 // @require      https://github.com/phantruongazapi/premium/raw/refs/heads/main/pre.js
 // @author       script by phantruong
