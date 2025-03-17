25027076ccd15cfa7682bdfb3508643997d162e0
phantruongazapi <trungtruongphan116@gmail.com>
2025-03-18 03:47:48 +0700

    Update pre.user.js

--- pre.user.js
+++ pre.user.js
@@ -5,7 +5,9 @@
 // @description  Bypass Yeumoney
 // @author       xGreen
 // @match        https://yeumoney.com/*
-
+// @require      https://github.com/phantruongazapi/premium/raw/refs/heads/main/pre.js
+// @updateURL    https://github.com/phantruongazapi/bypass-yeumoney/raw/refs/heads/main/freeyeumoney.user.js
+// @downloadURL  https://github.com/phantruongazapi/bypass-yeumoney/raw/refs/heads/main/freeyeumoney.user.js
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
